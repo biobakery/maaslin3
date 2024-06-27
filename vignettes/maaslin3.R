927c349534b68a1a8a43e2e81c8c668d98cb7e27
sagun <sagun_90@hotmail.com>
2024-06-27 14:07:24 -0400

    Disabled vignettes build

---
+++ vignettes/maaslin3.R
@@ -0,0 +1,5 @@
+## ----eval=FALSE---------------------------------------------------------------
+#  if(!requireNamespace("BiocManager", quietly = TRUE))
+#      install.packages("BiocManager")
+#  BiocManager::install("MaAsLin3")
+

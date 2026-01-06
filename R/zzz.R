fccd45a740955bac16dedb5acd02906a224348b9
Andrew Ghazi <6763470+andrewGhazi@users.noreply.github.com>
2026-01-06 11:53:50 -0500

    startup message using cli and utils

---
+++ R/zzz.R
@@ -0,0 +1,15 @@
+.onAttach = function(libname, pkgname) {
+    if (interactive()) {
+        
+        ver = utils::packageVersion("maaslin3")
+        
+        cli::cli_inform(paste0("This is MaAsLin3 version {.strong ", ver, "}"), 
+                        class = "packageStartupMessage")
+        
+        cli::cli_inform("{cli::symbol$bullet} {.strong Get help}: Visit the biobakery help forum at {.url https://forum.biobakery.org/}", 
+                        class = "packageStartupMessage")
+        
+        cli::cli_inform("{cli::symbol$bullet} {.strong Parallelize}: Enable {.fn mirai::daemons} as appropriate for your system.",
+                        class = "packageStartupMessage")
+    } 
+}
